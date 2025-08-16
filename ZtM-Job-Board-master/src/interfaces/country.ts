43902a4de42c7e3abec83166fdb487ea05857915
CerealKiller317 <shreyashir32@gmail.com>
2025-08-16 12:26:24 +0530

    Delete ZtM-Job-Board-master directory

--- ZtM-Job-Board-master/src/interfaces/country.ts
+++
@@ -1,8 +0,0 @@
-export default interface Country {
-    longitude: string
-    latitude: string
-    COUNTRY: string
-    ISO: string
-    COUNTRYAFF: string
-    AFF_ISO: string
-}
--- ZtM-Job-Board-master/src/util/useForceUpdate.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react'
-
-const useForceUpdate = () => {
-    const [value, setValue] = useState(0)
-    return () => setValue((prev) => prev + 1)
-}
-
-export default useForceUpdate
